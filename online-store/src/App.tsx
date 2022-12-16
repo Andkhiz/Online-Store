b400a20e98b7ab277d33a3432215027a30c2f240
viktorykings <kingsviktory@gmail.com>
2022-12-16 23:23:35 +0300

    refactor: change structure of the project

--- online-store/src/App.tsx
+++ online-store/src/App.tsx
@@ -1,28 +1,10 @@
 import React from 'react';
 import './App.scss';
-import Header from './components/Header';
-import MainInfo from './components/MainInfo';
-import MyFilter from './components/MyFilter';
-import MyInputRange from './components/MyInputRange';
+import MainPage from './components/MainPage';
 
 function App (): JSX.Element {
   return (
-    <div className="App">
-      <Header/>
-      <main>
-        <aside className='side-bar'>
-          <div className="side-buttons">
-            <button>Reset filter</button>
-            <button>Copy link</button>
-          </div>
-          <MyFilter title='Category'/>
-          <MyFilter title='brand'/>
-          <MyInputRange title='Price'/>
-          <MyInputRange title='Stock'/>
-        </aside>
-        <MainInfo/>
-      </main>
-    </div>
+    <MainPage/>
   );
 }
 export default App;
