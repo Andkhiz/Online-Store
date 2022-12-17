4a78c3b4586d98e078070407023c043350d154ec
viktorykings <kingsviktory@gmail.com>
2022-12-17 16:10:28 +0300

    feat: add structure of products page

--- online-store/src/pages/ProductPage.tsx
+++ online-store/src/pages/ProductPage.tsx
@@ -2,8 +2,30 @@ import React from 'react';
 
 function ProductPage (): JSX.Element {
   return (
-    <main>
-      <h1>product info coming soon</h1>
+    <main className='product'>
+      <div className="product-path">
+        store---smartphones---apple
+      </div>
+      <div className="product-container">
+        <div className="product-img-container">
+          <div className="aside-img"></div>
+          <div className="main-img"></div>
+        </div>
+        <div className="product-description-container">
+          <p>title</p>
+          <p>descr</p>
+          <p>discount</p>
+          <p></p>
+          <p></p>
+          <p></p>
+          <p></p>
+        </div>
+        <div className="product-purchase">
+          <p></p>
+          <button>add to cart</button>
+          <button>buy now</button>
+        </div>
+      </div>
     </main>
   );
 }
