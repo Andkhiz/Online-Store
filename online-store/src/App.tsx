fcfc21cd246dde8f3d6fa5759373800f8b5f1fc4
Andkhiz <andkhiz@gmail.com>
2022-12-17 10:45:56 +0200

    feat: add routing

--- online-store/src/App.tsx
+++ online-store/src/App.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 import './App.scss';
-import Header from './components/Header';
+import LayoutPage from './pages/LayoutPage';
 import MainPage from './pages/MainPage';
 import CartPage from './pages/CartPage';
 import ProductPage from './pages/ProductPage';
@@ -8,10 +10,16 @@ import ErrorPage from './pages/ErrorPage';
 
 function App (): JSX.Element {
   return (
-    <div className="App">
-      <Header/>
-      <CartPage/>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LayoutPage />}>
+          <Route index element={<MainPage />} />
+          <Route path="product" element={<ProductPage />} />
+          <Route path="cart" element={<CartPage />} />
+          <Route path="*" element={<ErrorPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 export default App;
