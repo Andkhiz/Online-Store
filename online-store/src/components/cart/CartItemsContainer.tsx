274cb410cfeef927640fabd8de9e01375ee39000
Andkhiz <andkhiz@gmail.com>
2023-01-04 23:54:20 +0200

    fix: onChange limit

--- online-store/src/components/cart/CartItemsContainer.tsx
+++ online-store/src/components/cart/CartItemsContainer.tsx
@@ -46,6 +46,9 @@ export default function CartItemsContainer ({ cartPageData, setCartPageData, tot
             <input className='cart-state__limit' type={'number'} min={1} max={cartPageData.length} value={generalCartData.limit}
             onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
               const newValue = e.target.value;
+              console.log('limit change');
+              console.log(newValue);
+              console.log(getCartQueryParams('limit', String(newValue), cartPageData.length));
               setSearchParams(getCartQueryParams('limit', String(newValue), cartPageData.length));
             }}
             />
--- online-store/src/controller/queryParams/queryCartParams.ts
+++ online-store/src/controller/queryParams/queryCartParams.ts
@@ -1,10 +1,14 @@
 export function getCartQueryParams (category: string, value: string, cartLength: number): string {
+  console.log(category);
+  console.log(value);
   let myPath = '';
   const oldQuery = window.location.href.indexOf('?') > 0 ? window.location.href.slice(window.location.href.indexOf('?') + 1) : '';
   const arr = oldQuery.split('&');
+  let use = false;
   myPath = arr.reduce((query, el) => {
     const myParam = el.split('=');
     if (myParam[0] === category) {
+      use = true;
       if (category === 'page') {
         query += `page=${value}&`;
       }
@@ -21,6 +25,12 @@ export function getCartQueryParams (category: string, value: string, cartLength:
     return query;
   }, '');
 
-  myPath = myPath.slice(0, -1);
+  console.log('----------');
+  console.log(myPath);
+  if (use) {
+    myPath = myPath.slice(0, -1);
+  } else {
+    myPath += category + '=' + value;
+  }
   return myPath;
 }
